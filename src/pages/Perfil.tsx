a75c30ae57de87049abf2485f5d15a0d1d8eec26
ClaytonEduard <clayton_eduard@hotmail.com>
2024-03-14 14:27:55 -0300

    Finalizando estilizacao pagina perfil inicial

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -2,7 +2,28 @@ import { Layout } from "../components/Layout/Layout";
 const Perfil = () => {
   return (
     <Layout>
-      <h1>Perfil</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Tech</th>
+            <th scope="col">Tipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>React</td>
+            <td>Frontend</td>
+          </tr>
+          <tr>
+            <td>Angular</td>
+            <td>Frontend</td>
+          </tr>
+          <tr>
+            <td>Node</td>
+            <td>Backend</td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   );
 };
