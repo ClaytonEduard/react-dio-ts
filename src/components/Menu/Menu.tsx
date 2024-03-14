975f41404201cc35235019d113dba2ab56b5df19
ClaytonEduard <clayton_eduard@hotmail.com>
2024-03-14 14:19:21 -0300

    Estilizando a pagina inicial

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <nav className="navbar navbar-light navbar-expand-lg bg-success">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to={"/"}>
+          <span className="ml-1 text-light">DIO Study</span>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Menu;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,26 @@ import { Layout } from "../components/Layout/Layout";
 const Home = () => {
   return (
     <Layout>
-      <h1>Home page</h1>
+      <div className="col-4 m-auto">
+        <h1>Login</h1>
+        <div className="mb-3">
+          <label htmlFor="gitUser" className="form-label">
+            Usuário do github
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="gitUser"
+            aria-describedby="userHelp"
+          />
+          <div id="userHelp" className="form-text">
+            Informe seu usuário do gitHub
+          </div>
+        </div>
+        <button type="button" className="btn btn-primary">
+          Entrar
+        </button>
+      </div>
     </Layout>
   );
 };
