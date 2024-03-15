d511ed92f1f860e727b5fda1113083384dbf3368
ClaytonEduard <clayton_eduard@hotmail.com>
2024-03-15 13:13:11 -0300

    Criando primeiros teste unitario e Componente Tabela

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Table = () => {
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Tech</th>
+          <th scope="col">Tipo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>React</td>
+          <td>Frontend</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
